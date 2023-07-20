26466b795977a92acd19c3e56b6f1266dfd68c5a
PedroHSSodre <pedrosodredev@gmail.com>
2023-07-20 20:32:07 -0300

    feat:  add icons

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,7 +1,10 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
 import Map from '../pages/Map';
 import Favorites from '../pages/Favorites';
+import { Ionicons } from '@expo/vector-icons';
+import theme from '../style/theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,14 +14,16 @@ function TabRoutes() {
         <Tab.Screen 
             name="Map" 
             options={{
-              title: "Mapa"
+              title: "Mapa",
+              tabBarIcon: () => <Ionicons name='home-outline' size={theme.fontSize.md}/>
             }}
             component={Map} 
         />
         <Tab.Screen 
             name="Favorites" 
             options={{
-              title: "Favoritos"
+              title: "Favoritos",
+              tabBarIcon: () => <Ionicons name='star-outline' size={theme.fontSize.md}/>
             }}
             component={Favorites} 
         />
