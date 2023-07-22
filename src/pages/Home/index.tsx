0633620dd8d7e3436d848bbd097ae27b5657a4ef
PedroHSSodre <pedrosodredev@gmail.com>
2023-07-22 00:25:40 -0300

    refactor: title text adjusted

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
-import Button from "../../components/Button";
 import { Container, Text, Title } from "./styled";
+
+import Button from "../../components/Button";
 
 function Home({ navigation }) {
     const handleNavigate = () => navigation.navigate("App");
 
     return (
         <Container>
-            <Title>Bem vindo ao Warker app!</Title>
+            <Title>Bem vindo ao Warker App!</Title>
             <Text>
                 No mundo pós-apocaliptico em que vivemos o combustível tem um valor inestimável. 
                 Gangues bárbaras lutam até a morte pelo controle desse valioso recurso e o app WARKER é a última esperança 
